06df05d67193745340d3332dcbd711a378e37169
0x002500 <tiger_pan@outlook.com>
2025-04-05 16:55:48 +0800

    [feat] add the get_pools_amount to the req_turbo.rs

--- src/request/req_turbos.rs
+++ src/request/req_turbos.rs
@@ -1,1 +1,11 @@
-const API_URL: &str = "https://api.turbos.finance/pools/v2?page=1&pageSize=20000&orderBy&category&coinTypes&includeRisk=false&symbol";
+use reqwest::blocking;
+use serde::Deserialize;
+use serde_json::Value;
+use crate::models::turbos::Turbos;
+
+fn get_pools_amount() -> i64 {
+    let api_url: &str = "https://api.turbos.finance/pools/v2?page=1&pageSize=5&orderBy&category&coinTypes&includeRisk=false&symbol";
+    let response: String = reqwest::blocking::get(api_url).unwrap().text().unwrap();
+    let pools: Turbos = serde_json::from_str(&response).unwrap();
+    pools.data.total
+}
