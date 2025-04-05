5591374323cf8e9bf743dd718c1f6ee2a62b69fa
0x002500 <tiger_pan@outlook.com>
2025-04-05 16:56:11 +0800

    [feat] add the get_pools function to the req_turbos.rs

--- src/request/req_turbos.rs
+++ src/request/req_turbos.rs
@@ -9,3 +9,10 @@ fn get_pools_amount() -> i64 {
     let pools: Turbos = serde_json::from_str(&response).unwrap();
     pools.data.total
 }
+
+pub fn get_pools() -> Turbos {
+    let api_url: String = format!("https://api.turbos.finance/pools/v2?page=1&pageSize={}&orderBy&category&coinTypes&includeRisk=false&symbol", get_pools_amount());
+    let response: String = reqwest::blocking::get(api_url).unwrap().text().unwrap();
+    let pools: Turbos = serde_json::from_str(&response).unwrap();
+    pools
+}
