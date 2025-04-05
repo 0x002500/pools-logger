dc2512ced70b3eee660542a98e2454e7e687c374
0x002500 <tiger_pan@outlook.com>
2025-04-05 16:48:18 +0800

    [feat] add function to cetus.rs to get the number of pools

--- src/models/cetus.rs
+++ src/models/cetus.rs
@@ -20,7 +20,7 @@ pub struct LpList {
     pub decimals: i64,
     pub fee: String,
     pub tick_spacing: String,
-    pub pool_type: PoolType,
+    pub pool_type: String,
     pub address: String,
     pub coin_a_address: String,
     pub coin_b_address: String,
@@ -32,19 +32,19 @@ pub struct LpList {
     pub rewarder_display3: bool,
     pub rewarder_display4: bool,
     pub rewarder_display5: bool,
-    pub labels: Option<Vec<Option<serde_json::Value>>>,
+    pub labels: Option<serde_json::Value>,
     pub coin_a: Coin,
     pub coin_b: Coin,
     pub price: String,
-    pub rewarder_usd: Vec<String>,
+    pub rewarder_usd: Vec<Option<serde_json::Value>>,
     pub rewarder_apr: Vec<String>,
     pub is_forward: bool,
     pub price_range_config: Option<serde_json::Value>,
     pub object: Object,
     pub category: String,
     pub is_vaults: bool,
-    pub stable_farming: Option<StableFarming>,
-    pub vaults: Option<Vec<String>>,
+    pub stable_farming: StableFarming,
+    pub vaults: Option<serde_json::Value>,
     pub pure_tvl_in_usd: String,
     #[serde(rename = "vol_in_usd_24h")]
     pub vol_in_usd_24_h: String,
@@ -52,7 +52,7 @@ pub struct LpList {
     pub fee_24__h: String,
     pub total_apr: String,
     pub apr: Apr,
-    pub extensions: Option<Extensions>,
+    pub extensions: Option<serde_json::Value>,
     pub has_mining: bool,
     pub has_farming: bool,
     pub no_incentives: bool,
@@ -74,25 +74,13 @@ pub struct Coin {
     pub logo_url: String,
     pub coingecko_id: String,
     pub project_url: String,
-    pub labels: Vec<Label>,
+    pub labels: Vec<Option<serde_json::Value>>,
     pub is_trusted: bool,
 }
 
 #[derive(Serialize, Deserialize)]
-pub enum Label {
-    Wormhole,
-}
-
-#[derive(Serialize, Deserialize)]
-pub struct Extensions {
-    pub show_rewarder_4: Option<String>,
-    pub show_rewarder_5: Option<String>,
-    pub zap: Option<String>,
-}
-
-#[derive(Serialize, Deserialize)]
 pub struct Object {
-    pub coin_a: f64,
+    pub coin_a: i64,
     pub coin_b: i64,
     pub tick_spacing: i64,
     pub fee_rate: i64,
@@ -105,45 +93,15 @@ pub struct Object {
 
 #[derive(Serialize, Deserialize)]
 pub struct RewarderManager {
-    pub fields: RewarderManagerFields,
+    pub fields: Fields,
 }
 
 #[derive(Serialize, Deserialize)]
-pub struct RewarderManagerFields {
-    pub rewarders: Vec<Rewarder>,
+pub struct Fields {
+    pub rewarders: Vec<Option<serde_json::Value>>,
     pub points_released: String,
     pub points_growth_global: String,
     pub last_updated_time: i64,
-}
-
-#[derive(Serialize, Deserialize)]
-pub struct Rewarder {
-    pub fields: RewarderFields,
-}
-
-#[derive(Serialize, Deserialize)]
-pub struct RewarderFields {
-    pub reward_coin: RewardCoin,
-    pub emissions_per_second: String,
-    pub growth_global: String,
-}
-
-#[derive(Serialize, Deserialize)]
-pub struct RewardCoin {
-    pub fields: RewardCoinFields,
-}
-
-#[derive(Serialize, Deserialize)]
-pub struct RewardCoinFields {
-    pub name: String,
-}
-
-#[derive(Serialize, Deserialize)]
-#[serde(rename_all = "snake_case")]
-pub enum PoolType {
-    #[serde(rename = "")]
-    Empty,
-    Unstable,
 }
 
 #[derive(Serialize, Deserialize)]
@@ -157,27 +115,6 @@ pub struct StableFarming {
     pub apr: String,
     pub effective_tick_lower: i64,
     pub effective_tick_upper: i64,
-    pub total_wrapped_amount: TotalWrappedAmount,
-    pub stable_rewarder: Option<Vec<StableRewarder>>,
-}
-
-#[derive(Serialize, Deserialize)]
-pub struct StableRewarder {
-    pub apr: String,
-    pub coin: String,
-    pub amount_second: String,
-    pub amount_usd: String,
-    pub symbol: String,
-    pub name: String,
-    pub decimals: i64,
-    #[serde(rename = "logoURI")]
-    pub logo_uri: String,
-}
-
-#[derive(Serialize, Deserialize)]
-pub enum TotalWrappedAmount {
-    #[serde(rename = "")]
-    Empty,
-    #[serde(rename = "1693687749723846")]
-    The1693687749723846,
+    pub total_wrapped_amount: String,
+    pub stable_rewarder: Option<serde_json::Value>,
 }
