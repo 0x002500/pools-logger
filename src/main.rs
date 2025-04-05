bb9913802ea5702a34756506547042b7a44b6c46
0x002500 <tiger_pan@outlook.com>
2025-04-05 16:42:55 +0800

    [feat] add logger to main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use log::{debug, error, log_enabled, info, Level};
+use serde::de;
+
+mod models;
+mod request;
+
 fn main() {
-    println!("Hello, world!");
+    env_logger::init();
+    info!("Starting up...");
 }
