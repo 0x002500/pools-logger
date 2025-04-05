8fa5c7c6e4e4b632c4f4795ca8c72ea79e765c0c
0x002500 <tiger_pan@outlook.com>
2025-04-05 10:25:56 +0800

    [feat] create request.rs for request data from api

---
+++ src/request.rs
@@ -0,0 +1,2 @@
+let cetus_api_url: &str = "https://api-sui.cetus.zone/v2/sui/stats_pools?is_vaults=false&display_all_pools=false&has_mining=true&has_farming=true&no_incentives=true&order_by=-totalApr&limit=20000&offset=0&coin_type&pool";
+let turbos_api_url: &str = "https://api.turbos.finance/pools/v2?page=1&pageSize=20000&orderBy&category&coinTypes&includeRisk=false&symbol";
