3a54a3f6151471124d7c408b10528ab09b6abb2c
0x002500 <tiger_pan@outlook.com>
2025-04-05 17:34:57 +0800

    [feat] add loggers to the req_*.rs

--- src/request/req_cetus.rs
+++ src/request/req_cetus.rs
@@ -1,10 +1,9 @@
-use reqwest::blocking;
-use serde::Deserialize;
-use serde_json::Value;
+use log::info;
 use crate::models::cetus::Cetus;
 
 fn get_pools_amount() -> i64 {
     let api_url: &str = "https://api-sui.cetus.zone/v2/sui/stats_pools?is_vaults=false&display_all_pools=false&has_mining=true&has_farming=true&no_incentives=true&order_by=-totalApr&limit=1&offset=0&coin_type&pool";
+    info!("Getting the number of pools from Cetus API");
     let response: String = reqwest::blocking::get(api_url).unwrap().text().unwrap();
     let pools: Cetus = serde_json::from_str(&response).unwrap();
     pools.data.total
@@ -12,6 +11,7 @@ fn get_pools_amount() -> i64 {
 
 pub fn get_pools() -> Cetus {
     let api_url: String = format!("https://api-sui.cetus.zone/v2/sui/stats_pools?is_vaults=false&display_all_pools=false&has_mining=true&has_farming=true&no_incentives=true&order_by=-totalApr&limit={}&offset=0&coin_type&pool", get_pools_amount());
+    info!("Getting the pools data from Cetus API");
     let response: String = reqwest::blocking::get(api_url).unwrap().text().unwrap();
     let pools: Cetus = serde_json::from_str(&response).unwrap();
     pools
--- src/request/req_turbos.rs
+++ src/request/req_turbos.rs
@@ -1,10 +1,9 @@
-use reqwest::blocking;
-use serde::Deserialize;
-use serde_json::Value;
+use log::info;
 use crate::models::turbos::Turbos;
 
 fn get_pools_amount() -> i64 {
     let api_url: &str = "https://api.turbos.finance/pools/v2?page=1&pageSize=5&orderBy&category&coinTypes&includeRisk=false&symbol";
+    info!("Getting the number of pools from Turbos Finance API");
     let response: String = reqwest::blocking::get(api_url).unwrap().text().unwrap();
     let pools: Turbos = serde_json::from_str(&response).unwrap();
     pools.data.total
@@ -12,6 +11,7 @@ fn get_pools_amount() -> i64 {
 
 pub fn get_pools() -> Turbos {
     let api_url: String = format!("https://api.turbos.finance/pools/v2?page=1&pageSize={}&orderBy&category&coinTypes&includeRisk=false&symbol", get_pools_amount());
+    info!("Getting the pools data from Turbos Finance API");
     let response: String = reqwest::blocking::get(api_url).unwrap().text().unwrap();
     let pools: Turbos = serde_json::from_str(&response).unwrap();
     pools
