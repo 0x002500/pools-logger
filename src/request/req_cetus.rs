b16a797379073b28d780f86037f77a9e7d33f6de
0x002500 <tiger_pan@outlook.com>
2025-04-05 16:53:06 +0800

    [feat] add the get_pools function to req_cetus.rs

--- src/request/req_cetus.rs
+++ src/request/req_cetus.rs
@@ -3,11 +3,16 @@ use serde::Deserialize;
 use serde_json::Value;
 use crate::models::cetus::Cetus;
 
-
-const API_URL: &str = "https://api-sui.cetus.zone/v2/sui/stats_pools?is_vaults=false&display_all_pools=false&has_mining=true&has_farming=true&no_incentives=true&order_by=-totalApr&limit=1&offset=0&coin_type&pool";
-
-pub fn get_pools_amount() -> i64 {
-    let response = reqwest::blocking::get(API_URL).unwrap().text().unwrap();
+fn get_pools_amount() -> i64 {
+    let api_url: &str = "https://api-sui.cetus.zone/v2/sui/stats_pools?is_vaults=false&display_all_pools=false&has_mining=true&has_farming=true&no_incentives=true&order_by=-totalApr&limit=1&offset=0&coin_type&pool";
+    let response: String = reqwest::blocking::get(api_url).unwrap().text().unwrap();
     let pools: Cetus = serde_json::from_str(&response).unwrap();
     pools.data.total
+}
+
+pub fn get_pools() -> Cetus {
+    let api_url: String = format!("https://api-sui.cetus.zone/v2/sui/stats_pools?is_vaults=false&display_all_pools=false&has_mining=true&has_farming=true&no_incentives=true&order_by=-totalApr&limit={}&offset=0&coin_type&pool", get_pools_amount());
+    let response: String = reqwest::blocking::get(api_url).unwrap().text().unwrap();
+    let pools: Cetus = serde_json::from_str(&response).unwrap();
+    pools
 }
