159e6d163324d3d6d937a3381b393d42a4d95096
0x002500 <tiger_pan@outlook.com>
2025-04-10 18:29:50 +0800

    [refactor] update write_to_file function and improve error handling in cetus function

--- src/job.rs
+++ src/job.rs
@@ -1,10 +1,12 @@
-use crate::models::{cetus::Cetus, turbos::Turbos};
+use crate::models::{turbos::Turbos};
 use crate::request::{req_cetus, req_turbos};
 use serde::{Deserialize, Serialize};
-use std::fs::File;
-use std::io::{BufWriter, Result};
+use std::fs;
+use std::fs::OpenOptions;
+use std::io::{self, BufWriter, Result, Write};
 use serde_json;
 use chrono::Local;
+use std::path::{Path, PathBuf};
 
 #[derive(Serialize, Deserialize, Clone)]
 struct PoolInfo {
@@ -20,61 +22,68 @@ struct PoolsCollection {
     pools: Vec<PoolInfo>,
 }
 
-fn write_to_file(pools_collection: &PoolsCollection, path: &str) -> Result<()> {
-    let file = File::create(path)?;
-    let buf_writer = BufWriter::with_capacity(4 * 1024 * 1024, file);
+fn write_to_file(pools_collection: &PoolsCollection, timestamp: &str) -> io::Result<()> {
+    let mut path = PathBuf::from("data");
+    fs::create_dir_all(&path)?;
+    path.push(format!("{}.json", timestamp));
 
-    serde_json::to_writer(buf_writer, pools_collection)?;
-
+    let file = OpenOptions::new()
+        .write(true)
+        .create(true)
+        .open(&path)?;
+    let mut buf_writer = BufWriter::new(file);
+    serde_json::to_writer(&mut buf_writer, pools_collection)?;
+    buf_writer.flush()?;
     Ok(())
 }
 
 fn cetus() -> PoolsCollection {
-    let mut pools_collection: PoolsCollection = PoolsCollection { pools: Vec::new() };
-    let pools: Cetus = req_cetus::get_pools();
-    for pool in pools.data.lp_list {
-        let pool_info: PoolInfo = PoolInfo {
-            symbol: pool.symbol,
-            total_apr: pool.total_apr,
-            liquidity: pool.pure_tvl_in_usd,
-            vol_in_usd_24_h: pool.vol_in_usd_24_h,
-            fee_24_h: pool.fee_24__h,
-        };
-
-        pools_collection.pools.push(pool_info);
+    let mut pools_collection = PoolsCollection { pools: Vec::new() };
+    match req_cetus::get_pools() {
+        Ok(pools) => {
+            for pool in pools.data.lp_list {
+                let pool_info = PoolInfo {
+                    symbol: pool.symbol,
+                    total_apr: pool.total_apr,
+                    liquidity: pool.pure_tvl_in_usd,
+                    vol_in_usd_24_h: pool.vol_in_usd_24_h,
+                    fee_24_h: pool.fee_24__h, // 注意：可考虑更正字段名
+                };
+                pools_collection.pools.push(pool_info);
+            }
+        }
+        Err(e) => eprintln!("获取池数据时出错: {}", e),
     }
     pools_collection
 }
 
 fn turbos() -> PoolsCollection {
-    let mut pools_collection: PoolsCollection = PoolsCollection { pools: Vec::new() };
+    let mut pools_collection = PoolsCollection { pools: Vec::new() };
     let pools: Turbos = req_turbos::get_pools();
     for pool in pools.data.list {
-        let symbol: String = format!("{}-{}", pool.coin_symbol_a, pool.coin_symbol_b);
-        let pool_info: PoolInfo = PoolInfo {
-            symbol: symbol,
+        let symbol = format!("{}-{}", pool.coin_symbol_a, pool.coin_symbol_b);
+        let pool_info = PoolInfo {
+            symbol,
             total_apr: pool.apr.to_string(),
             liquidity: pool.liquidity_usd.to_string(),
             vol_in_usd_24_h: pool.volume_24_h_usd.to_string(),
             fee_24_h: pool.fee_24_h_usd.to_string(),
         };
-
         pools_collection.pools.push(pool_info);
     }
     pools_collection
 }
 
 pub fn job() -> Result<()> {
-    let cetus_pools: PoolsCollection = cetus();
-    let turbos_pools: PoolsCollection = turbos();
-    let pools_collection: PoolsCollection = PoolsCollection {
+    let cetus_pools = cetus();
+    let turbos_pools = turbos();
+    let pools_collection = PoolsCollection {
         pools: [cetus_pools.pools.clone(), turbos_pools.pools.clone()].concat(),
     };
 
     let local_time = Local::now();
-    let timestamp = local_time.format("%Y-%m-%d %H:%M:%S%.3f").to_string();
-    let path: String = format!("./data/{}.json", timestamp);
-    write_to_file(&pools_collection, &path)?;
+    let timestamp = local_time.format("%Y-%m-%d_%H-%M-%S_%3f").to_string();
 
+    write_to_file(&pools_collection, &timestamp)?;
     Ok(())
 }
