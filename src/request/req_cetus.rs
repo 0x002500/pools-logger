d07eb3619370437e7a2fcf306ebe3780d9a1077a
0x002500 <tiger_pan@outlook.com>
2025-04-12 17:22:00 +0800

    [feat] specify types for client and response in get_pools_amount and get_pools functions

--- src/request/req_cetus.rs
+++ src/request/req_cetus.rs
@@ -7,8 +7,8 @@ fn get_pools_amount() -> Result<i64, Box<dyn Error>> {
     let api_url = "https://api-sui.cetus.zone/v2/sui/stats_pools?is_vaults=false&display_all_pools=false&has_mining=true&has_farming=true&no_incentives=true&order_by=-totalApr&limit=1&offset=0&coin_type&pool";
     info!("Getting the number of pools from Cetus's API");
 
-    let client = Client::new();
-    let response = client.get(api_url).send()?;
+    let client: Client = Client::new();
+    let response: reqwest::blocking::Response = client.get(api_url).send()?;
 
     if !response.status().is_success() {
         error!("Error handel, Status Code: {}", response.status());
@@ -29,15 +29,15 @@ pub fn get_pools() -> Result<Cetus, Box<dyn Error>> {
     );
     info!("Getting data from Cetus's API");
 
-    let client = Client::new();
-    let response = client.get(&api_url).send()?;
+    let client: Client = Client::new();
+    let response: reqwest::blocking::Response = client.get(&api_url).send()?;
 
     if !response.status().is_success() {
         error!("Error handel, Status Code: {}", response.status());
         return Err(format!("Error handel, Status Code: {}", response.status()).into());
     }
 
-    let response_text = response.text()?;
+    let response_text: String = response.text()?;
     let pools: Cetus = serde_json::from_str(&response_text)?;
 
     Ok(pools)
